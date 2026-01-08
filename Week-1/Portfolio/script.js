adf7a99cddda258707942c93fd5d97e61b05868d
Verkan <vedmeharia@gmail.com>
2026-01-08 18:21:35 +0530

    Removed pop-up feature, added auto email upon form submission

--- Week-1/Portfolio/script.js
+++ Week-1/Portfolio/script.js
@@ -11,34 +11,44 @@ window.addEventListener('scroll',()=>{
 });
 
 const form=document.getElementById('contactForm');
-console.log(form);
-const closeBtn=document.getElementById('backtohome');
-const modal=document.querySelector('.modal');
+// console.log(form);
+// const closeBtn=document.getElementById('backtohome');
+// const modal=document.querySelector('.modal');
 form.addEventListener("submit",async(e)=>{
     e.preventDefault();
-    console.log("Form Submitted");
-    for (let pair of new FormData(form).entries()) {
-    console.log(pair[0] + ': ' + pair[1]);
-    }
-    const response=await fetch(form.action,{
-        method:form.method,
-        body:new FormData(form),
-        headers:{
-            "Accept":"application/json"
-        }
-    });
-    console.log("Status:",response.status); 
+    window.location.reload();});
+//     for (let pair of new FormData(form).entries()) {
+//     console.log(pair[0] + ': ' + pair[1]);
+//     }
+//     const response=await fetch(form.action,{
+//         method:form.method,
+//         body:new FormData(form),
+//         headers:{
+//             "Accept":"application/json"
+//         }
+//     });
+//     console.log("Status:",response.status); 
 
-    if(response.ok){
-        modal.classList.add("active");
-        form.reset();
-    }else{
-        const data=await response.json();
-        alert("Oops! There was a problem submitting your form");
-        console.log("errors:",data);
-    }
-});
+//     if(response.ok){
+//         modal.classList.add("active");
+//         form.reset();
+//     }else{
+//         const data=await response.json();
+//         alert("Oops! There was a problem submitting your form");
+//         console.log("errors:",data);
+//     }
+// });
 
-closeBtn.addEventListener("click",()=>{
-    modal.classList.remove("active");
-});
+// closeBtn.addEventListener("click",()=>{
+//     modal.classList.remove("active");
+// });
+
+function sendMail(){
+    let parms={
+        name: document.getElementById("namef").value,
+        email: document.getElementById("EmailID").value,
+        title: document.getElementById("message").value,
+    }
+
+    emailjs.send("service_hj3ye8h","template_t9q1o1v",parms).then(alert("Email sent successfully!")) 
+}
